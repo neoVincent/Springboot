5ec82cb0eb1caa5cdf97501f288496c3711c5d21
vincent <neovincent@163.com>
2018-04-09 09:48:20 +0800

    Add log and colourful log output

--- demo/src/main/java/com/hack/demo/controller/HelloController.java
+++ demo/src/main/java/com/hack/demo/controller/HelloController.java
@@ -2,13 +2,11 @@ package com.hack.demo.controller;
 
 import com.hack.demo.domain.City;
 import com.hack.demo.service.CityService;
-import com.hack.demo.service.impl.CityServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,6 +17,8 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String index() {
+        Logger log = LoggerFactory.getLogger(HelloController.class);
+        log.info("========w==============");
         return "Hello World!!!";
     }
 
@@ -48,6 +48,7 @@ public class HelloController {
     @RequestMapping(value = "api/updateCity")
     public String updateCity(@RequestBody City record) {
         cityService.updateByPrimaryKey(record);
+
         return "updateUser";
     }
 
